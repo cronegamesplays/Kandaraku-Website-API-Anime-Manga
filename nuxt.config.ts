d4805b76622ae1c66ed8148228d8aa00faa23d3b
CroneGamesPlays <felipebragabr123@gmail.com>
2024-03-28 15:39:45 -0300

    fix 3 nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 // Importação correta de defineNuxtConfig
 import { defineNuxtConfig } from '@nuxt/config';
 
@@ -13,24 +14,3 @@ export default defineNuxtConfig({
     '~/global.css'
   ],
 })
-
-/*import { defineComponent } from '@nuxtjs/composition-api';
-
-export default defineComponent({
-  head() {
-    return {
-      title: 'Kandaraku Beta v1',
-      meta: [
-        { hid: 'description', name: 'description', content: 'Assista seus animes online gratuitamente ou acesse o banco de dados de diversos animes disponíveis pelo Kandaraku API.' },
-        { hid: 'og:url', property: 'og:url', content: 'https://v1-kandaraku-beta.vercel.app/' },
-        { hid: 'og:type', property: 'og:type', content: 'website' },
-        { hid: 'og:title', property: 'og:title', content: 'Kandaraku Beta v1' },
-        { hid: 'og:description', property: 'og:description', content: 'Assista seus animes online gratuitamente ou acesse o banco de dados de diversos animes disponíveis pelo Kandaraku API.' },
-        { hid: 'og:image', property: 'og:image', content: '/favicon.ico' },
-        { hid: 'og:image:alt', property: 'og:image:alt', content: 'Kandaraku Logo' },
-        { hid: 'og:locale', property: 'og:locale', content: 'pt_BR' },
-        { hid: 'og:site_name', property: 'og:site_name', content: 'Kandaraku' }
-      ]
-    };
-  }
-})*/
