526f14b8f4455286c0ae727f1a24934da2ddf390
CroneGamesPlays <felipebragabr123@gmail.com>
2024-03-27 23:57:24 -0300

    upload commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  head: {
+    titleTemplate: '%s - Kandaraku',
+    title: 'Kandaraku',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Assista seus animes online gratuitamente ou acesse o banco de dados de diversos animes disponiveis pelo Kandaraku API.' },
+      { property: 'og:url', content: 'https://v1-kandaraku-beta.vercel.app/' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:title', content: 'Kandaraku Beta v1' },
+      { property: 'og:description', content: 'Assista seus animes online gratuitamente ou acesse o banco de dados de diversos animes disponiveis pelo Kandaraku API.' },
+      { property: 'og:image', content: '/favicon.ico' }, // Remove o diretório 'public' da imagem
+      { property: 'og:image:alt', content: 'Kandaraku Logo' },
+      { property: 'og:locale', content: 'pt_BR' },
+      { property: 'og:site_name', content: 'Kandaraku' }
+    ],
+  },
   devtools: { enabled: true },
   modules: ["nuxt-icon", "@nuxt/image", "@pinia/nuxt"],
   css: [
