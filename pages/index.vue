5ddcb305ee40969e68e4c2975e07bad6dc6665d6
CroneGamesPlays <felipebragabr123@gmail.com>
2024-03-29 18:08:48 -0300

    add banner home page

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,9 @@
         class="wallpaper"
         src="https://iili.io/JjOFKg4.jpg"
     />
+    <div class="banner">
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/banner/151807-37yfQA3ym8PA.jpg" alt="Banner" class="banner-image">
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -15,5 +18,22 @@
     background-color: rgba(0, 0, 0, 0.5);
     /* Aplica um filtro de desfoque */
     filter: blur(3px);
+}
+.banner {
+  position: fixed;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 80%; /* largura do banner */
+  height: 200px; /* altura do banner */
+  border-radius: 40px;
+  overflow: hidden;
+  z-index: 999;
+}
+
+.banner-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* mantém a proporção da imagem */
 }
 </style>
