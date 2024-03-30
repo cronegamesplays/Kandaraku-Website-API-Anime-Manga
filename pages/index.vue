bf45bae3d76c3ff798ef4ae2e3bf93874a897b96
CroneGamesPlays <felipebragabr123@gmail.com>
2024-03-30 01:01:40 -0300

    banner update

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,55 @@
-<script>
-</script>
-
 <template>
-    <NuxtImg
-        class="wallpaper"
-        src="https://iili.io/JjOFKg4.jpg"
-    />
-    <div class="banner">
-    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/banner/151807-37yfQA3ym8PA.jpg" alt="Banner" class="banner-image">
-  </div>
+    <div class="wallpaper-container">
+        <NuxtImg class="wallpaper" src="https://iili.io/JjOFKg4.jpg" />
+        <div class="banner">
+            <img src="https://s4.anilist.co/file/anilistcdn/media/anime/banner/151807-37yfQA3ym8PA.jpg" alt="Banner" class="banner-image" />
+            <div class="banner-content">
+                <h1 style="font-family: 'Open Sans', sans-serif; font-size: 2rem;">Título do Banner</h1>
+                <p style="font-family: 'Open Sans', sans-serif; font-size: 1rem;">Descrição do Banner por cima da imagem</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.wallpaper-container {
+    position: relative;
+}
 .wallpaper {
     width: 100%;
-    /* Adiciona um fundo preto semi-transparente */
     background-color: rgba(0, 0, 0, 0.5);
-    /* Aplica um filtro de desfoque */
     filter: blur(3px);
 }
 .banner {
-  position: fixed;
-  top: 20px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 80%; /* largura do banner */
-  height: 200px; /* altura do banner */
-  border-radius: 40px;
-  overflow: hidden;
-  z-index: 999;
+    position: absolute;
+    top: 50px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 95%;
+    height: 500px;
+    border-radius: 40px;
+    overflow: hidden;
+    z-index: 999;
 }
-
 .banner-image {
-  width: 100%;
-  height: 100%;
-  object-fit: cover; /* mantém a proporção da imagem */
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+.banner-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    text-align: center;
+    z-index: 1000;
+}
+.banner-content h1 {
+    font-size: 2rem;
+    margin: 0;
+}
+.banner-content p {
+    font-size: 1rem;
 }
 </style>
