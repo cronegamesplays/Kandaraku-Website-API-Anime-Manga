9e7b2eecfac2ce946be5616547dbd144997cfb50
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-08-27 10:57:59 -0300

    feature: Created a simple navbar for the mobile screens

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,3 +1,7 @@
+import ThemeTogglerButton from "@/components/theme-toggler-button";
+import { Button } from "@/components/ui/button";
+import { Menu, Search } from "react-feather";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,11 +9,22 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <header className="border-b sticky top-0 backdrop-blur">
-        <div className="container mx-auto p-3">
-          <h1 className="font-bold text-3xl">
-            K<span className="text-purple-600">an</span>daraku
-          </h1>
+      <header className="border-b sticky top-0 bg-zinc-900">
+        <div className="container mx-auto p-3 flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Button size="icon" variant="ghost" className="bg-zinc-800 text-white">
+              <Menu className="size-[1.2rem]" />
+            </Button>
+            <h1 className="font-bold text-2xl sm:text-3xl text-purple-600">
+              Kandaraku
+            </h1>
+          </div>
+          <div className="flex gap-2">
+            <Button size="icon" variant="ghost" className="bg-zinc-800 text-white">
+              <Search className="size-[1.2rem]" />
+            </Button>
+            <ThemeTogglerButton />
+          </div>
         </div>
       </header>
       <main>
