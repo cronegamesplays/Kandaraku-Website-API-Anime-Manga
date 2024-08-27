ae2974b24457466f2006bfbbcb5e4ce423a0c877
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-08-26 21:43:30 -0300

    prune: Removed the dead code from the initial project template

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "simolex.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
