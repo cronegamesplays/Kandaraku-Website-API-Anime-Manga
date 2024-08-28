e6ec56dfb717d197437603c0e1e60bdb1b69f9d4
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-08-28 15:40:02 -0300

    mv: The CSS files should be stored in it's own folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "next-themes";
 
